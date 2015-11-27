5128737ad18246ca076909309e126986312e96fd
k-lange <mail@klange.org>
2015-11-27 20:41:52 +0100

    add code

---
+++ index.js
@@ -0,0 +1,62 @@
+var gm = require('gm');
+var loaderUtils = require('loader-utils');
+var mime = require('mime');
+
+module.exports = loader;
+module.exports.raw = true;
+
+function loader (content) {
+    this.cacheable();
+    var callback = this.async();
+    var url = emitFile(this, content);
+
+    Promise.all([
+        getSizePromise(),
+        getResizePromise.then(getDataUrl.bind(null, this.resourcePath))
+    ]).then(function (values) {
+        var size = values[0];
+        var placeholder = values[1];
+        var output = {
+            width: size.width,
+            height: size.height,
+            placeholder: placeholder,
+            url: url
+        };
+
+        callback('module.exports = ' + JSON.stringify(output));
+    });
+}
+
+function getSizePromise(content) {
+    return new Promise(function (resolve, reject) {
+        gm(content).size(function (err, size) {
+            if (err) { reject(err); }
+            resolve(size);
+        });
+    });
+}
+
+function getResizePromise(content) {
+    return new Promise(function (resolve, reject) {
+        gm(content).resize(30).toBuffer(function (err, buffer) {
+            if (err) { reject(err); }
+            resolve(buffer);
+        });
+    });
+}
+
+function emitFile(context, content) {
+    var url = loaderUtils.interpolateName(context, '[hash].[ext]', {
+        context: context.options.context,
+        content: content
+    });
+
+    context.emitFile(url, content);
+
+    return url;
+}
+
+function getDataUrl(path, content) {
+    var mimeType = mime.lookup(path);
+    return 'data:' +  (mimetype ? mimetype + ';' : '') + 'base64,' + content.toString('base64');
+}
